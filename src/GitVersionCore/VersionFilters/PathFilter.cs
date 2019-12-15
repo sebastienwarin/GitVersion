f0d6a64d5131890c536142de2e15e2a8553c6fee
Sébastien Warin <sebastien@warin.fr>
2019-12-15 22:57:06 +0100

    Do not exclude commit tagged with the tag-prefix

--- src/GitVersionCore/VersionFilters/PathFilter.cs
+++ src/GitVersionCore/VersionFilters/PathFilter.cs
@@ -22,14 +22,24 @@ namespace GitVersion.VersionFilters
         public bool Exclude(BaseVersion version, out string reason)
         {
             if (version == null) throw new ArgumentNullException(nameof(version));
+            return Exclude(version.BaseVersionSource, version.Context, out reason);
+        }
+
+        public bool Exclude(Commit commit, GitVersionContext context, out string reason)
+        {
+            if (commit == null) throw new ArgumentNullException(nameof(commit));
 
             reason = null;
 
-            if (version.BaseVersionSource != null)
+            var match = new System.Text.RegularExpressions.Regex($"^({context.Configuration.GitTagPrefix})?.*$",
+                System.Text.RegularExpressions.RegexOptions.Compiled);
+
+            if (commit != null &&
+                !context.Repository.Tags.Any(t => t.Target.Sha == commit.Sha && match.IsMatch(t.FriendlyName)))
             {
-                Tree commitTree = version.BaseVersionSource.Tree; // Main Tree
-                Tree parentCommitTree = version.BaseVersionSource.Parents.FirstOrDefault()?.Tree; // Secondary Tree
-                Patch patch = version.Context.Repository.Diff.Compare<Patch>(parentCommitTree, commitTree); // Difference
+                Tree commitTree = commit.Tree; // Main Tree
+                Tree parentCommitTree = commit.Parents.FirstOrDefault()?.Tree; // Secondary Tree
+                Patch patch = context.Repository.Diff.Compare<Patch>(parentCommitTree, commitTree); // Difference
                 switch (mode)
                 {
                     case PathFilterMode.Inclusive:
